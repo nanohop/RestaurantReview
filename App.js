8c3a131278f8994ab24c29f6ec5ebd53a0cb4d0d
Chris Achard <chrisachard@gmail.com>
2018-10-04 13:46:16 -0400

    Display and format text header and list items

--- App.js
+++ App.js
@@ -1,34 +1,33 @@
 import React, {Component} from 'react';
-import {StyleSheet, Text, View} from 'react-native';
 
+import {
+  View,
+  Text
+} from 'react-native'
 
 export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Restaurant Review
+      <View>
+        <Text style={{
+          padding: 40,
+          fontSize: 30,
+          textAlign: 'center',
+          color: '#0066CC',
+          fontWeight: '300'
+        }}>Restaurant Review</Text>
+
+        <Text>React Cafe</Text>
+        <Text style={{ color: 'grey'}}>
+          123 Anywhere St
         </Text>
+
+        <Text>Fancy Restaurant</Text>
+        <Text style={{ color: 'grey'}}>
+          799 Main St
+        </Text>
+
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
