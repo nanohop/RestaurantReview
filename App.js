083416c21d9a49ec2f26dbb0453fba74e372488b
Chris Achard <chrisachard@gmail.com>
2018-10-04 14:05:29 -0400

    Style components with StyleSheet

--- App.js
+++ App.js
@@ -2,8 +2,11 @@ import React, {Component} from 'react';
 
 import {
   View,
-  Text
+  Text,
+  StyleSheet
 } from 'react-native'
+
+import HeaderStyle from './HeaderStyle'
 
 const restaurants = [
   {name: 'React Cafe', address: '123 Anywhere St'},
@@ -17,41 +20,25 @@ export default class App extends Component {
       <View style={{
         flex: 1
       }}>
-        <Text style={{
-          padding: 40,
-          fontSize: 30,
-          textAlign: 'center',
-          color: '#0066CC',
-          fontWeight: '300'
-        }}>Restaurant Review</Text>
+        <Text style={HeaderStyle.header}>Restaurant Review</Text>
 
         {
           restaurants.map((place, index) => {
             return (
-              <View key={place.name} style={{
-                flexDirection: 'row'
-              }}>
-                <View style={{
-                  flex: 1,
-                  alignItems: 'center',
-                  justifyContent: 'center'
-                }}>
+              <View key={place.name} style={[
+                styles.row,
+                { backgroundColor: index % 2 === 0 ? 'white' : '#F3F3F7' }
+              ]}>
+                <View style={styles.edges}>
                   <Text>{index + 1}</Text>
                 </View>
 
-                <View style={{
-                  flexDirection: 'column',
-                  flex: 8
-                }}>
+                <View style={styles.nameAddress}>
                   <Text>{place.name}</Text>
-                  <Text style={{color: 'grey'}}>{place.address}</Text>
+                  <Text style={styles.addressText}>{place.address}</Text>
                 </View>
 
-                <View style={{
-                  flex: 1,
-                  alignItems: 'center',
-                  justifyContent: 'center'
-                }}>
+                <View style={styles.edges}>
                   <Text>Info</Text>
                 </View>
               </View>
@@ -63,3 +50,22 @@ export default class App extends Component {
     );
   }
 }
+
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: 'row'
+  },
+  edges: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 5
+  },
+  nameAddress: {
+    flexDirection: 'column',
+    flex: 8
+  },
+  addressText: {
+    color: 'grey'
+  }
+})
---
+++ HeaderStyle.js
@@ -0,0 +1,12 @@
+
+import { StyleSheet } from 'react-native'
+
+export default StyleSheet.create({
+  header: {
+    padding: 40,
+    fontSize: 30,
+    textAlign: 'center',
+    color: '#0066CC',
+    fontWeight: '300'
+  },
+})
